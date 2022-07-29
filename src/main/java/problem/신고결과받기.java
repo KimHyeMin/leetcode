4ef863d078c0444017a0fb5971ad29237ed3599a
apples1309 <apples1309@gmail.com>
2022-07-29 18:05:08 +0900

    taewhan version

--- src/main/java/problem/신고결과받기.java
+++ src/main/java/problem/신고결과받기.java
@@ -1,6 +1,8 @@
 package problem;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class 신고결과받기 {
     public int[] solution(String[] id_list, String[] report, int k) {
@@ -35,41 +37,76 @@ public class 신고결과받기 {
 //        }
 //        return answer;
 
-        Map<String, Integer> index = new HashMap<>();
-        int[] count = new int[id_list.length];
-        boolean[][] reportArr= new boolean[id_list.length][id_list.length];
+//        Map<String, Integer> index = new HashMap<>();
+//        int[] count = new int[id_list.length];
+//        boolean[][] reportArr= new boolean[id_list.length][id_list.length];
+//
+//        for (int i = 0 ; i<id_list.length;i++) {
+//            index.put(id_list[i], i);
+//        }
+//
+//        for (String line : report) {
+//            String[] split = line.split(" ");
+//            String from = split[0];
+//            String to = split[1];
+//
+//            int fromIdx = index.get(from);
+//            int toIdx = index.get(to);
+//
+//            if (reportArr[fromIdx][toIdx]) {
+//                continue;
+//            }
+//            reportArr[fromIdx][toIdx] = true;
+//            count[toIdx] = count[toIdx] + 1;
+//        }
+//
+//        int[] answer = new int[id_list.length];
+//
+//        for (int i = 0; i < id_list.length ; i++) {
+//            int cnt = 0;
+//            for (int j =0 ;j<id_list.length;j++) {
+//                if (i == j) continue;
+//                if (reportArr[i][j] && count[j]>=k) {
+//                    cnt ++;
+//                }
+//            }
+//            answer[i] = cnt;
+//        }
+//        return answer;
 
-        for (int i = 0 ; i<id_list.length;i++) {
-            index.put(id_list[i], i);
-        }
+        int[] answer = new int[id_list.length];
+        Map<String, Integer> reporterIndex = IntStream
+                .range(0, id_list.length)
+                .boxed()
+                .collect(Collectors.toMap(
+                    index -> id_list[index],
+                    index -> index
+                ));
+
+        Map<String, Set<String>> reportRecords = new HashMap<>();
 
         for (String line : report) {
             String[] split = line.split(" ");
-            String from = split[0];
-            String to = split[1];
+            String newReporter = split[0];
+            String reportTarget = split[1];
 
-            int fromIdx = index.get(from);
-            int toIdx = index.get(to);
+            if (reportRecords.containsKey(reportTarget) == false) {
+                reportRecords.put(reportTarget, new HashSet<>());
+            }
 
-            if (reportArr[fromIdx][toIdx]) {
+            Set<String> reporterSet = reportRecords.get(reportTarget);
+            if (reporterSet.contains(newReporter)) {
                 continue;
             }
-            reportArr[fromIdx][toIdx] = true;
-            count[toIdx] = count[toIdx] + 1;
-        }
+            reporterSet.add(newReporter);
 
-        int[] answer = new int[id_list.length];
-
-        for (int i = 0; i < id_list.length ; i++) {
-            int cnt = 0;
-            for (int j =0 ;j<id_list.length;j++) {
-                if (i == j) continue;
-                if (reportArr[i][j] && count[j]>=k) {
-                    cnt ++;
-                }
+            if (reporterSet.size() == k) {
+                reporterSet.forEach(reporter -> ++answer[reporterIndex.get(reporter)]);
+            } else if (reporterSet.size() > k) {
+                ++answer[reporterIndex.get(newReporter)];
             }
-            answer[i] = cnt;
         }
+
         return answer;
     }
 
